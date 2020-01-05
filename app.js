3e3b8c05408a7b233b2e7950e0d93097862582e4
gardenk11181 <55073876+gardenk11181@users.noreply.github.com>
2020-01-05 13:51:21 +0900

    We can drawing

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
 
+canvas.width = 550;
+canvas.height = 550;
+
 ctx.strokeStyle = "#2c2c2c";
 ctx.lineWidth = 2.5;
 
